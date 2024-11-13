2eb7dc91f96e488b898923576066af2e9d8d2728
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-11-14 00:52:30 +0900

    chore: API Gateway 모듈 생성

---
+++ gateway-service/src/main/java/me/ustory/api/gateway/GatewayServiceApplication.java
@@ -0,0 +1,9 @@
+package me.ustory.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
