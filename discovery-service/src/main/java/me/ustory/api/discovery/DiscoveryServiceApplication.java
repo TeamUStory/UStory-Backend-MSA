e9116a468bd970deff2b9108205efe471e009c9c
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-11-13 21:52:17 +0900

    chore: Discovery Service 모듈 생성

---
+++ discovery-service/src/main/java/me/ustory/api/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,9 @@
+package me.ustory.api.discovery;
+
+import org.springframework.boot.SpringApplication;
+
+public class DiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
