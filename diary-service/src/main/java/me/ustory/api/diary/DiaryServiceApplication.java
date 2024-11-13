80f87ff053b917f6a6fe31a8980812de5a3768fe
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-11-13 21:29:55 +0900

    project init

---
+++ diary-service/src/main/java/me/ustory/api/diary/DiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package me.ustory.api.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryServiceApplication.class, args);
+    }
+
+}
---
+++ paper-service/src/main/java/me/ustory/api/paper/PaperServiceApplication.java
@@ -0,0 +1,13 @@
+package me.ustory.api.paper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperServiceApplication.class, args);
+    }
+
+}
