7f2a8319649be49170ebe918aefa75758f5804c1
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-11-17 23:40:01 +0900

    refactor: JPA Auditing 애너테이션을 Config로 분리

--- paper-service/src/main/java/me/ustory/api/paper/PaperServiceApplication.java
+++ paper-service/src/main/java/me/ustory/api/paper/PaperServiceApplication.java
@@ -3,11 +3,9 @@ package me.ustory.api.paper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableJpaAuditing
 public class PaperServiceApplication {
 
     public static void main(String[] args) {
